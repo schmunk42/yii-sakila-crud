8928253b3df6409064085d5aad2d14748020f066
Tobias Munk <schmunk@usrbin.de>
2013-07-29 18:58:09 +0200

    code formatting

--- migrations/m130729_144420_init_sakila.php
+++ migrations/m130729_144420_init_sakila.php
@@ -2,28 +2,28 @@
 
 class m130729_144420_init_sakila extends EDbMigration
 {
-	public function up()
-	{
-        $schema = file_get_contents(dirname(__FILE__).'/sakila-schema.sql');
-	    $this->execute($schema);
-        $data = file_get_contents(dirname(__FILE__).'/sakila-data.sql');
-	    $this->execute($data);
-	}
+    public function up()
+    {
+        $schema = file_get_contents(dirname(__FILE__) . '/sakila-schema.sql');
+        $this->execute($schema);
+        $data = file_get_contents(dirname(__FILE__) . '/sakila-data.sql');
+        $this->execute($data);
+    }
 
-	public function down()
-	{
-		echo "m130729_144420_init_sakila does not support migration down.\n";
-		return false;
-	}
+    public function down()
+    {
+        echo "m130729_144420_init_sakila does not support migration down.\n";
+        return false;
+    }
 
-	/*
-	// Use safeUp/safeDown to do migration with transaction
-	public function safeUp()
-	{
-	}
+    /*
+    // Use safeUp/safeDown to do migration with transaction
+    public function safeUp()
+    {
+    }
 
-	public function safeDown()
-	{
-	}
-	*/
+    public function safeDown()
+    {
+    }
+    */
 }
