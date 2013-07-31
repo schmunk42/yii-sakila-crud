6a360e7db5b17df30fe7b8b191c185d5884d6919
Tobias Munk <schmunk@usrbin.de>
2013-07-31 20:58:52 +0200

    updated config

--- giic-config.php
+++ giic-config.php
@@ -1,5 +1,8 @@
 <?php
+// GIIC CONFIG FILE
+// ----------------
 
+// define table list (eg. you don't need MANY_MANY tables)
 $tables = array(
     'actor',
     'address',
@@ -16,14 +19,17 @@ $tables = array(
     'customer',
     'country'
 );
+
+// define CRUDs
 $cruds  = $tables;
 
+// init actions
 $actions = array();
 
 // generate default models
 foreach ($tables AS $table) {
     $actions[] = array(
-        "template" => "FullModel",
+        "codeModel" => "FullModelCode",
         "generator"=> 'vendor.phundament.gii-template-collection.fullModel.FullModelGenerator',
         "templates"=> array(
             'default' => dirname(__FILE__) . '/../../../vendor/phundament/gii-template-collection/fullModel/templates/default',
@@ -31,7 +37,7 @@ foreach ($tables AS $table) {
         "model"    => array(
             "tableName"  => $table,
             "modelClass" => ucFirst($table),
-            "modelPath"  => "sakila.models",
+            "modelPath"  => "sakila.models.gtc",
             "template"   => "default"
         )
     );
@@ -40,30 +46,30 @@ foreach ($tables AS $table) {
 // generate slim CRUDs into application
 foreach ($cruds AS $crud) {
     $actions[] = array(
-        "template" => "FullCrud",
+        "codeModel" => "FullCrudCode",
         "generator"=> 'vendor.phundament.gii-template-collection.fullCrud.FullCrudGenerator',
         "templates"=> array(
             'slim' => dirname(__FILE__) . '/../../../vendor/phundament/gii-template-collection/fullCrud/templates/slim',
         ),
         "model"    => array(
-            "model"      => "sakila.models." . ucfirst($crud),
+            "model"      => "sakila.models.gtc." . ucfirst($crud),
             "controller" => $crud,
             "template"   => "slim"
         )
     );
 }
 
-// generate legacy CRUDs into application subfolder zakila
+// generate legacy CRUDs into module subfolder legacy
 foreach ($cruds AS $crud) {
     $actions[] = array(
-        "template" => "FullCrud",
+        "codeModel" => "FullCrudCode",
         "generator"=> 'vendor.phundament.gii-template-collection.fullCrud.FullCrudGenerator',
         "templates"=> array(
             'legacy' => dirname(__FILE__) . '/../../../vendor/phundament/gii-template-collection/fullCrud/templates/legacy',
         ),
         "model"    => array(
-            "model"      => "sakila.models." . ucfirst($crud),
-            "controller" => "legacy/" . $crud,
+            "model"      => "sakila.models.gtc." . ucfirst($crud),
+            "controller" => "sakila/legacy/" . $crud,
             "template"   => "legacy"
         )
     );
@@ -72,13 +78,13 @@ foreach ($cruds AS $crud) {
 // generate slim CRUDs into module sakila subfolder slim
 foreach ($cruds AS $crud) {
     $actions[] = array(
-        "template" => "FullCrud",
+        "codeModel" => "FullCrudCode",
         "generator"=> 'vendor.phundament.gii-template-collection.fullCrud.FullCrudGenerator',
         "templates"=> array(
             'slim' => dirname(__FILE__) . '/../../../vendor/phundament/gii-template-collection/fullCrud/templates/slim',
         ),
         "model"    => array(
-            "model"      => "sakila.models." . ucfirst($crud),
+            "model"      => "sakila.models.gtc." . ucfirst($crud),
             "controller" => "sakila/slim/" . $crud,
             "template"   => "slim"
         )
@@ -88,35 +94,52 @@ foreach ($cruds AS $crud) {
 // generate hybrid CRUDs into module sakila subfolder hybrid
 foreach ($cruds AS $crud) {
     $actions[] = array(
-        "template" => "FullCrud",
+        "codeModel" => "FullCrudCode",
         "generator"=> 'vendor.phundament.gii-template-collection.fullCrud.FullCrudGenerator',
         "templates"=> array(
             'hybrid' => dirname(__FILE__) . '/../../../vendor/phundament/gii-template-collection/fullCrud/templates/hybrid',
         ),
         "model"    => array(
-            "model"      => "sakila.models." . ucfirst($crud),
+            "model"      => "sakila.models.gtc." . ucfirst($crud),
             "controller" => "sakila/hybrid/" . $crud,
             "template"   => "hybrid"
         )
     );
 }
 
-/*
-foreach ($cruds AS $crud) {
+// generate giix models
+foreach ($tables AS $table) {
     $actions[] = array(
-        "template" => "giixModel",
+        "codeModel" => "GiixModelCode",
         "generator"=> 'application.extensions.giix.generators.giixModel.GiixModelGenerator',
         "templates"=> array(
             'default' => dirname(__FILE__) . '/../../../app/extensions/giix/generators/giixModel/templates/default',
         ),
         "model"    => array(
-            "model"      => "sakila.models.giix." . ucfirst($crud),
-            "controller" => "sakila/giix/" . $crud,
-            "template"   => "default",
+            "tableName"  => $table,
+            "modelClass" => ucFirst($table),
+            "baseClass"  => "GxActiveRecord",
+            "modelPath"  => "sakila.models.giix",
+            "template"   => "default"
         )
     );
 }
-*/
+
+// generate giix CRUDs into module sakila subfolder giix
+foreach ($cruds AS $crud) {
+    $actions[] = array(
+        "codeModel" => "GiixCrudCode",
+        "generator"=> 'application.extensions.giix.generators.giixCrud.GiixCrudGenerator',
+        "templates"=> array(
+            'default' => dirname(__FILE__) . '/../../../app/extensions/giix/generators/giixCrud/templates/default',
+        ),
+        "model"    => array(
+            "model"      => "sakila.models.gtc." . ucfirst($crud),
+            "controller" => "sakila/giix/" . $crud,
+            "template"   => "default"
+        )
+    );
+}
 
 return array(
     "actions" => $actions
